b453ee159ac40d0c0ee8a9d40e850a9dd0e37fd6
54242001 <54242001@L815WD220022.edu.cbe.ab.ca>
2022-04-11 11:35:28 -0600

    DOne

--- c8/src/Account/Account.java
+++ c8/src/Account/Account.java
@@ -6,26 +6,26 @@ public class Account {
 	private Customer cust;
 		
 	
-
+    // Giving all the strings 
 	public Account(double bal, String fName, String lName, String str, String city, String st, String zip) {
 		balance = bal;
 		cust = new Customer(fName, lName, str, city, st, zip);
 	}
 	
 
-
+       //Set balance, get balance
 	public double getBalance() {
 	 	return(balance);
 	}
 
 
-
+     //set deposit,get deposit
 	public void deposit(double amt) {
 	 	balance += amt;
 	}
 
 	
-
+       //Withdraw and if not enough balance show "Not enough money in account"
 	public void withdrawal(double amt) {
 	 	if (amt <= balance) {
 	 		balance -= amt;
@@ -35,7 +35,7 @@ public class Account {
 	}
 	
 	
-	
+	//Return Strings
 	public String toString() {
 		String accountString;
 		NumberFormat money = NumberFormat.getCurrencyInstance();
