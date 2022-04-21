9249b4f7ca056b04a1defe44aa7a7a80a7a66d9c
54242001 <54242001@L815WD220022.edu.cbe.ab.ca>
2022-04-21 10:21:26 -0600

    DOne

---
+++ Chapter11/src/MyFilePart2.java
@@ -0,0 +1,4 @@
+
+public class MyFilePart2 {
+
+}
---
+++ Chapter11/src/TheFileClass.java
@@ -0,0 +1,29 @@
+
+import java.io.*;
+import java.util.Scanner;
+ public class TheFileClass {
+
+	 public static void main(String[] args) { 
+ 
+		 System.out.println("File Location");   //Asks user for file location
+		 String Rd; //String which takes in whatever the user types
+		 Scanner FL = new Scanner(System.in);//collects user input 
+		 Rd = FL.next(); 
+		 File f1;
+         f1 = new File(Rd);
+
+  if (f1.exists()) {
+ 
+	  System.out.println("File already exists.");
+  } else {
+
+	  System.out.println("File does not exist.");
+         }
+                                            }
+                          }
+
+
+
+
+
+
