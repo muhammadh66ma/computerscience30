064e3c695332bfaa5ab7a33854a8cb929ebca392
54242001 <54242001@L815WD220022.edu.cbe.ab.ca>
2022-04-25 11:11:20 -0600

    done

--- Chapter11/src/MyFilePart2.java
+++ Chapter11/src/MyFilePart2.java
@@ -1,4 +1,59 @@
 
+import java.io.*;
+import java.util.Scanner;
+
 public class MyFilePart2 {
 
-}
+ public static void main(String[] args) {
+      File textFile = new File("C:\\Users\\54242001\\Documents\\n1.txt");// reads the certain file 
+         if (textFile.exists()) {                         // if statment for if file exists  
+             System.out.println("File already exists.");
+       } else {
+         try {
+        textFile.createNewFile();
+         System.out.println("New file created.");
+           } catch (IOException e) {
+               System.out.println("File could not be created.");
+   System.err.println("IOException: " + e.getMessage());
+    
+           }
+          }
+          Scanner SC=new Scanner(System.in);
+  		
+  		//Asks user to type d to delete file
+  		System.out.println("\n Type d to delete the file. Type any other character to keep the file: ");
+  		
+  		
+  		char input=SC.next().charAt(0); // allows 1 character to be stored ex: d
+  		
+  		
+  		if(input=='d')
+  {
+   
+  		if(textFile.delete())//if file is deleted successfully
+         
+  		{
+            System.out.println("File deleted successfully");
+         
+  		}
+         
+  		else
+         
+  		{
+             
+  		System.out.println("Failed to delete the file");
+         
+  		}  
+         }
+
+  		else   //if CH is not d
+
+  		{
+    
+  			System.out.println("File kept because you did not type D");
+
+  		}
+
+
+           }
+              }
