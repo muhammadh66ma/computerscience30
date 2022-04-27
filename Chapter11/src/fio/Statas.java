0ff1ffeafba466237a0378de42d63a3795d64229
54242001 <54242001@L815WD220022.edu.cbe.ab.ca>
2022-04-27 11:37:32 -0600

    done

--- Chapter11/src/fio/Statas.java
+++ Chapter11/src/fio/Statas.java
@@ -5,37 +5,45 @@ import java.util.Scanner;
 public class Statas {
 	 
 	 public static void main(String[] args) {
-	 File dataFile = new File("C:\\Users\\54242001\\Documents\\Suraj.txt");
-	 
-	 FileWriter out;
-	 BufferedWriter writeFile;
-	 Scanner input = new Scanner(System.in);
-	 double score;
-	 String name;
+	 File dataFile = new File("C:\\Users\\54242001\\Documents\\test1.dat");
+	 FileReader in;
+	 BufferedReader readFile;
+	 String score;
+	 double avgScore;
+	 double totalScores = 0;
+	 int numScores = 0;
+	 double HighestScore;
+	 double LowestScore;
 
 	 try {
-	 out = new FileWriter(dataFile);
-    writeFile = new BufferedWriter(out);
-	 for (int i = 0; i < 5; i++) {
-	 System.out.print("Enter student name: ");
-	 name = input.next();
-	 System.out.print("Enter test score: ");
-	 score = input.nextDouble();
-	
-	 writeFile.write(name);
-	 writeFile.newLine();
-	 writeFile.write(String.valueOf(score));
-	 writeFile.newLine();
-	
-	     }
-	    writeFile.close();
-	    out.close();
+	 in = new FileReader(dataFile);
+	 readFile = new BufferedReader(in);
+	 while ((score = readFile.readLine()) != null ) {
+	 numScores += 1;
+	 System.out.println(score);
+	 totalScores += Double.parseDouble(score);
+	 }
 	 
-	    System.out.println("Data written to file.");
+	 
+	 
+	 avgScore = totalScores / numScores;
+	 System.out.println("Average = " + avgScore);
+	 readFile.close();
+	 in.close();
 	 } 
+	 
+	 
+	 catch (FileNotFoundException e) {
+	 System.out.println("File does not exist or could not be found.");
+	 System.err.println("FileNotFoundException: "
+	 + e.getMessage());
+	 } 
+	 
+	 
 	 catch (IOException e) {
-	 System.out.println("Problem writing to file.");
+	 System.out.println("Problem reading file.");
 	 System.err.println("IOException: " + e.getMessage());
 	 }
+	 
 	 }
 	 }
