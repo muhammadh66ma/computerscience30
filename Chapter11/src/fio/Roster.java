15c743b9ee5a11793c06bf147baa905c33ee35ad
54242001 <54242001@L815WD220022.edu.cbe.ab.ca>
2022-05-10 09:18:07 -0600

    Done

--- Chapter11/src/fio/Roster.java
+++ Chapter11/src/fio/Roster.java
@@ -1,32 +1,111 @@
 package fio;
 
+//import the required packages
 
 import java.io.*;
-public class Roster implements Serializable {
- private String stuName;
- private double stuGrade;
 
- /**
- * constructor
- * pre: none
- * post: A Student object has been created.
- * Student data has been initialized with parameters.
- */
- public Student(String name, double grade)
- {
- stuName = name;
- stuGrade = grade;
- }
- /**
- * Creates a string representing the student object
- * pre: none
- * post: A string representing the student object
- * has been returned.
- */
- public String toString() {
- String stuString;
+import java.util.ArrayList;
 
- stuString = stuName + " grade: " + stuGrade;
- return(stuString);
- }
+import java.util.Scanner;
+
+//define the class ObjectWriteRead
+
+public class Roster
+
+{
+
+//define main function
+
+public static void main(String[] args)
+
+    {
+
+	Scanner scanner = new Scanner(System.in);
+	String fileName = null;
+	int numStudents;
+	String firstName;
+	String lastName;
+	stuName[] writeStudents = null;
+	stuName[] readStudents = null;
+	File file = null;
+	FileOutputStream out = null;
+	ObjectOutputStream writeStu = null;
+	FileInputStream in = null;
+	ObjectInputStream readStu = null;
+
+	// Assign
+	System.out.print("Enter file name: ");
+	fileName = scanner.nextLine();
+	file = new File(fileName);
+
+	System.out.print("Enter number of students: ");
+	numStudents = scanner.nextInt();
+	
+	scanner.nextLine();
+
+	
+
+	// Write StuName object
+	try {
+
+		out = new FileOutputStream(file);
+		writeStu = new ObjectOutputStream(out);
+		
+		for (int i = 1; i <= numStudents; i++) {
+            
+			//prompt user to enter name of user 
+			System.out.print("\nEnter first name of student " + i + ": ");
+			firstName = scanner.nextLine();
+			//prompt user to enter last name of user
+			System.out.print("Enter last name of student " + i + ": ");
+			lastName = scanner.nextLine();
+
+			writeStu.writeObject(new stuName(firstName, lastName));
+
+		}
+
+	
+		writeStu.close();
+
+		System.out.println("Data has been written to the file");
+		
+		//Read and diplay objects
+		
+		in = new FileInputStream(file);
+		readStu = new ObjectInputStream(in);
+		
+		for (int i = 1; i <= numStudents; i++) 
+		{
+			 System.out.println((stuName)readStu.readObject());
+		 }
+		readStu.close();
+		
+	} catch (FileNotFoundException e) {
+		System.out.println("File not found.");
+	} catch (IOException e) {
+		System.out.println("Problem with input/output.");
+	} catch (ClassNotFoundException e) {
+		System.out.println("Class could not be found to cast object.");
+	}
+
+	
+
+	
+		
+		
+
+	
+	
+	
+	
+	
+	
+	}
+	
+	
+
 }
+
+
+
+
---
+++ Chapter11/src/fio/stuName.java
@@ -0,0 +1,81 @@
+package fio;
+
+import java.io.Serializable;
+
+public class stuName implements Serializable {
+
+	/**
+	 * This id is used in the serialization process across different platforms.
+	 */
+	
+	
+	//Attributes/ Fields
+	private String firstName;
+	
+	private String lastName;
+
+	
+	// Constructors
+	
+	/**
+	 * Default constructor
+	 */
+	public stuName() {
+		super();
+	}
+
+
+	
+	public stuName(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+
+	
+	 // return the firstName
+	 
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	
+	 // set firstname
+	 
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	
+	 //return the lastName
+	 
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	// lastName to set
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	@Override
+	public String toString() {
+		return (firstName+ " " + lastName);
+	}
+	
+	
+	
+	}
+	
+	
+
+
+
+
+
+
